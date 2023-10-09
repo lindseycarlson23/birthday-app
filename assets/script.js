9e597257b3ba47f94395478b511e9e9d3d3f4695
clvalada <clvalada@gmail.com>
2023-10-09 14:42:15 -0400

    added comment tags to HTML, JS, and CSS files. Organized and revised the user instructions on the index file.

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,7 @@
+// function to collect and store user's birthday in local storage
 function storeBirthday() {
     const birthdayInput = document.getElementById('birthday').value;
     localStorage.setItem('userBirthday', birthdayInput);
+    // This resulting display is a placeholder that can be replaced with our API results
     document.getElementById('result').textContent = 'Happy Birthday!';
 }
