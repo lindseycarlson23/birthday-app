48ac5b5ed8ff9b86c6d7069b00c876276d50a75a
lindseycarlson23 <144490541+lindseycarlson23@users.noreply.github.com>
2023-10-10 21:40:40 -0500

    added billboard results to HTML dynamically

--- assets/script.js
+++ assets/script.js
@@ -30,30 +30,66 @@ function storeBirthday() {
              console.log(data);
             }); 
 
-    // I moved this to the bottom so it can go after both API calls and we can input the display code above it   
-    // This resulting display is a placeholder that can be replaced with our API results     
-    document.getElementById('result').textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Odio ut sem nulla pharetra diam sit amet. Sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi. Pharetra pharetra massa massa ultricies. Diam quam nulla porttitor massa id neque aliquam. Duis tristique sollicitudin nibh sit amet. Nisl pretium fusce id velit ut. Eget lorem dolor sed viverra ipsum nunc. Ac feugiat sed lectus vestibulum mattis. Proin fermentum leo vel orci porta non. Leo a diam sollicitudin tempor id eu nisl nunc. Neque gravida in fermentum et. Lacus vel facilisis volutpat est velit egestas. Nunc consequat interdum varius sit amet mattis vulputate enim';
 
-}
+  
+        var week = birthdayData; 
+        var bbContainer = document.getElementById("result-billboard");
+        var bbUrl = 'https://billboard-api5.p.rapidapi.com/api/charts/hot-100?week='+week;
+
+        fetch(bbUrl, {
+            headers: {
+            'X-RapidAPI-Key': '7e8f3dc8bbmsh74725bf383a10d3p10ec8ejsn093b73e95f52',
+            }
+            })
+            .then(function(response) {
+            return response.json();
+            })
+
+            .then(function(data){
+                var title = data.chart.entries[0].title;
+                var artist = data.chart.entries[0].artist;
+
+
+                bbContainer.innerHTML = "Number 1 on Billboard 100 was "+"'"+title+","+"'"+" by "+artist+"."
+                
+                
+            })
+            
+
+//document getELementbyID
+// innerText or innerHTML
+
+            // var chart = data;
+            // chart = data.chart;
+            //     console.log(chart)
+
+            // var keys = object.keys("chart")
+
+
+            ///I'm getting chart object and I need entries array
+        
+            // .then(function(data) {
+            //     console.log(data);
+            //     for (var i=0; i < 5; i++) {
+            //         var bbEvent = document.createElement('li');
+            //         bbEvent.innerText = data[i];
+            //     }
+            //     bbContainer.append(bbEvent);
+            // })
+            
+       
+    
+}; 
 
 //Billboard 100 API call
-    fetch('https://billboard-api5.p.rapidapi.com/api/charts/hot-100', {
-        headers: {
-        'X-RapidAPI-Key': '7e8f3dc8bbmsh74725bf383a10d3p10ec8ejsn093b73e95f52',
-        }
-        
-    })
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-    });
+    
+                
+    
+            
 
 
-// function billboardResults () {
-//     var week = 
-// }
-//how to get user input into the api call
-//loop through responses and limit to 5 with that
+
+// I moved this to the bottom so it can go after both API calls and we can input the display code above it   
+    // This resulting display is a placeholder that can be replaced with our API results     
+    // document.getElementById('result').textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Odio ut sem nulla pharetra diam sit amet. Sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi. Pharetra pharetra massa massa ultricies. Diam quam nulla porttitor massa id neque aliquam. Duis tristique sollicitudin nibh sit amet. Nisl pretium fusce id velit ut. Eget lorem dolor sed viverra ipsum nunc. Ac feugiat sed lectus vestibulum mattis. Proin fermentum leo vel orci porta non. Leo a diam sollicitudin tempor id eu nisl nunc. Neque gravida in fermentum et. Lacus vel facilisis volutpat est velit egestas. Nunc consequat interdum varius sit amet mattis vulputate enim';
 
